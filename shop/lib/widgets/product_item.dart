20f35e6fbbc21e9c878a9bf2bd384665f6580438
chrislenguyen <lnakhuong@gmail.com>
2020-01-10 13:05:28 +0700

    add items and items grid

--- shop/lib/widgets/product_item.dart
+++ shop/lib/widgets/product_item.dart
@@ -3,18 +3,18 @@ import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
-
+//int hello = 0;
 class ProductItem extends StatelessWidget {
   // final String id;
   // final String title;
   // final double price;
   // final String imageUrl;
-
   // ProductItem(this.id, this.title, this.price, this.imageUrl);
-
+  
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
+    //print(hello);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -33,13 +33,15 @@ class ProductItem extends StatelessWidget {
         header: Text(product.title),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+            ),
           ),
           title: Text(
             product.price.toString(),
