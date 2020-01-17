fe88376a6cdf97d1a4f19145a6548d5f8c56478f
chrislenguyen <lnakhuong@gmail.com>
2020-01-17 23:50:30 +0700

    add fav and all transit

--- shop/lib/screens/products_overview_screen.dart
+++ shop/lib/screens/products_overview_screen.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../providers/products.dart';
 
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatelessWidget { 
 
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Shop'), 
+        title: Text('Shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                productsContainer.showFavoritesOnly();
+              }
+              else {
+                productsContainer.showAll();
+              };
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Show Favorites'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
       body: ProductsGrid(),
     );
   }
 }
-
-
